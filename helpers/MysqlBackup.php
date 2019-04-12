da5323d3e26db7a374dcfb57ad26fce91374350a
klikar3 <klikar3@users.noreply.github.com>
2019-04-12 18:52:33 +0200

    -- do not backup data found via VIEWS

--- helpers/MysqlBackup.php
+++ helpers/MysqlBackup.php
@@ -62,7 +62,7 @@ if (version_compare(PHP_VERSION, '7.1', '<=')) {
     		unlink ( $this->file_name );
     	}
     	public function getTables($dbName = null) {
-    		$sql = 'SHOW TABLES';
+    		$sql = "SHOW FULL TABLES where Table_Type != 'VIEW'";
     		$cmd = Yii::$app->db->createCommand ( $sql );
     		$tables = $cmd->queryColumn ();
     		return $tables;
